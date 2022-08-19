0852f9482154cea743c1c26ab1b1b931ca6a10bb
bruno <brunobaptista9317@gmail.com>
2022-08-19 19:05:01 -0300

    salvando exercicio 6]

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = numbers[0]
+let quantidadeImpar = 0;
 
-for(let index = 1; index < numbers.length; index++){
-    if(numbers[index] > maiorNumero){
-        maiorNumero = numbers[index]
+for(let index = 0; index < numbers.length; index++){
+    if(numbers[index] % 2 != 0){
+        quantidadeImpar = quantidadeImpar +1;
     }
 }
-console.log(maiorNumero)
+if(quantidadeImpar > 0){
+    console.log('Quantidade de valores impares =', quantidadeImpar);
+}else{
+    console.log('nenhum valor ímpar encontrado');
+}
