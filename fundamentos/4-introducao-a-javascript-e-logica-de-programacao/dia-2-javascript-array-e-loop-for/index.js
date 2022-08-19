c5dd068a947860edd32c7ccc92420f47ea6d372d
bruno <brunobaptista9317@gmail.com>
2022-08-19 18:29:25 -0300

    salvando exercicio 4

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,8 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
+let media;
 
 for(let index = 0; index < numbers.length; index++){
     // console.log(numbers[index])
     soma = soma + numbers[index];
 }
-console.log(soma / numbers.length);
+media = soma / numbers.length
+
+if(media > 20){
+    console.log('valor maior que 20');
+}else{
+    console.log('valor menor ou igual 20');
+}
