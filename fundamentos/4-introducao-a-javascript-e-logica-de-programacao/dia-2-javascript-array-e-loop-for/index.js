989c7fddffc518d44ee166d021227873addd94d0
bruno <brunobaptista9317@gmail.com>
2022-08-19 19:20:38 -0300

    salvando exercicio 9

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -4,3 +4,7 @@ for(let index = 0; index < 25; index++){
     numbers.push(index + 1)
 }
 console.log(numbers) 
+
+for(let index2 = 0; index2 < numbers.length; index2++){
+    console.log(numbers[index2] / 2);
+}
