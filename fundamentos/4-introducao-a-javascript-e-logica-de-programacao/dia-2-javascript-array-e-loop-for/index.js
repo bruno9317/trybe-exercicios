55a22789302c4cf4a585633f3046bc6474fc6b9e
bruno <brunobaptista9317@gmail.com>
2022-08-19 18:24:34 -0300

    salvando exercicio 3

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -5,4 +5,4 @@ for(let index = 0; index < numbers.length; index++){
     // console.log(numbers[index])
     soma = soma + numbers[index];
 }
-console.log(soma);
+console.log(soma / numbers.length);
