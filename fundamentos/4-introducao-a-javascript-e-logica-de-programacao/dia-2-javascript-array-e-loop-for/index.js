ec8e2c4de8a7fc20cb70973ed67df9b18fc94ec7
bruno <brunobaptista9317@gmail.com>
2022-08-19 19:15:43 -0300

    salvando exercicio 7

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,9 +1,6 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = numbers[0];
+let numbers = [];
 
-for(let index = 1; index < numbers.length; index++){
-    if(numbers[index] < maiorNumero){
-        maiorNumero = numbers[index]
-    }
+for(let index = 0; index < 25; index++){
+    numbers.push(index + 1)
 }
-console.log(maiorNumero) 
+console.log(numbers) 
