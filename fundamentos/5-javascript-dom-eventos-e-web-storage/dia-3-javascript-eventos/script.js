542dfdd67505c8c63035a5f4a32b34042869f59e
bruno <brunobaptista9317@gmail.com>
2022-08-29 19:39:36 -0300

    salvando exercicio 1

--- fundamentos/5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,36 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  
+
+  function createDaysOfTheMonth(){
+    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const lista = document.getElementById('days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1){
+    let dia = decemberDaysList[index];
+    let diasDoMes = document.createElement('li');
+    diasDoMes.innerHTML = dia;
+    lista.appendChild(diasDoMes);
+  }
+}
+
+function addClasseDayAndHoliday(){
+  let listaDeFilhos = document.getElementById('days').children;
+  for (let index = 0; index < listaDeFilhos.length; index += 1){
+    if(listaDeFilhos[index].innerText === '24' || listaDeFilhos[index].innerText === '31'){
+      listaDeFilhos[index].className = "day holiday";
+    }else if(listaDeFilhos[index].innerText === '25'){
+      listaDeFilhos[index].className = "day holiday friday";
+    }else if (listaDeFilhos[index].innerText === '4' || listaDeFilhos[index].innerText === '11'|| listaDeFilhos[index].innerText === '18'){
+      listaDeFilhos[index].className = "day friday";
+    }else {
+      listaDeFilhos[index].className = "day1";
+    }
+  }
+}
+
+createDaysOfTheMonth();
+addClasseDayAndHoliday();
+
+  
